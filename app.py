1e864d4a97feb60662d01df3d2415994db69a061
SajithJude <judesajith.aj@gmail.com>
2022-12-03 19:30:36 +0530

    dsd

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import streamlit.components.v1 as components
 # >>> fig = px.box(range(10))
 # >>> fig.write_html('test.html')
 
-st.header("test html import")
+# st.header("test html import")
 
 HtmlFile = open("text.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
-components.html(source_code)
+components.html(source_code, width=1200, height=720)
